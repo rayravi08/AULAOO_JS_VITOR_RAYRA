343861abad62d65271adfb2e9c70f66149126654
rayravi08 <vitor.rayra123@gmail.com>
2024-11-13 14:41:35 -0300

    Add files via upload

---
+++ AULA00_JS_VITOR_RAYRA/script.js
@@ -0,0 +1,15 @@
+//alert("Testando o script");
+//caixa de texto no navegador
+
+var nome = prompt("Qual seu nome? ");
+//caixa de ação para inserir nome
+
+var sobrenome = prompt("Qual seu sobrenome? ");
+
+console.log("Olá "+nome+" "+sobrenome);
+
+document.write("<h1>Bem vindo "+nome+" "+sobrenome+"</h1>");
+//saida no DOM(aparecer na tela do usuario)
+
+document.write("Steve através de Javascript")
+document.write("<img src='https://sujeitoprogramador.com/steve.png' alt='foto do steve'/>")
